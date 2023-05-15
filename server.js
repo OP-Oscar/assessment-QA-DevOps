cb3e890db2d71f5fc387c084a326bcb66f522c01
OP-Oscar <oscarpena07@outlook.com>
2023-05-15 15:27:09 -0500

    update

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 // app.use(express.static(`${__dirname}/public`))
-app.use(express.static(`public`))
+app.use(express.static(`public`));
 
 
 // Add up the total health of all the robots
